76133db7dbc0bf8c2fe4db90c8d1f63e5125e067
Reid Miller <reidmiller999@gmail.com>
2020-05-29 16:24:25 -0700

    fixes

---
+++ client/build/precache-manifest.180abb5572211da16d4a1c14059ec977.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb18b5177640b107ad85d7170593066",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f01252016ba2277ff44c",
+    "url": "/static/css/2.a00eef2d.chunk.css"
+  },
+  {
+    "revision": "1adaa13447fc79f20bfe",
+    "url": "/static/css/main.e390e5c8.chunk.css"
+  },
+  {
+    "revision": "f01252016ba2277ff44c",
+    "url": "/static/js/2.8913a125.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8913a125.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1adaa13447fc79f20bfe",
+    "url": "/static/js/main.2bd5f230.chunk.js"
+  },
+  {
+    "revision": "76a6ac423fd57d9cd0a5",
+    "url": "/static/js/runtime-main.beab3704.js"
+  },
+  {
+    "revision": "6e5d6096c02e8c18ad95d40ae5bf0089",
+    "url": "/static/media/loading.6e5d6096.gif"
+  },
+  {
+    "revision": "88161edd0d92cf9eb207f2de84588385",
+    "url": "/static/media/not.88161edd.jpg"
+  }
+]);
